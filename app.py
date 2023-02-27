e85ba65bff30daa30e9ca80aed86f64b21fc718d
shivasaivemula <122033771+shivasaivemula@users.noreply.github.com>
2023-02-27 15:32:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("Shivasai vemula ")
+st.write("check out this [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
 st.snow()
 
 btn_click = st.button("Click Me!")
