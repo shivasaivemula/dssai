35e4e697726824cf16bff099b1c4c01dce3d473a
shivasaivemula <122033771+shivasaivemula@users.noreply.github.com>
2023-02-27 16:02:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title("Shivasai vemula ")
-st.write("check out this [link](https://github.com/shivasaivemula)")
-st.write("check out this [link](https://www.linkedin.com/in/shivasai-vemula-306169264)")
+st.write("check out this github[link](https://github.com/shivasaivemula)")
+st.write("check out this linkedin[link](https://www.linkedin.com/in/shivasai-vemula-306169264)")
 st.snow()
 
 btn_click = st.button("Click Me!")
